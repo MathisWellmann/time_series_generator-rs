41467993f0d6a01bdac6abecdc01be894c48d94f
MathisWellmann <wellmannmathis@gmail.com>
2025-03-07 10:55:23 +0100

    add `flake.nix` with lock file and all necessary dependencies for a development environment

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Flake for time_series_generator";
+
+  inputs = {
+    nixpks.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = (
+          pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+            targets = ["x86_64-unknown-linux-gnu"];
+          }
+        );
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              rust
+              taplo
+              fontconfig
+              pkg-config
+            ];
+          };
+        }
+    );
+}
